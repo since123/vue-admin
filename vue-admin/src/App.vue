af280037be927820b10b4ed91d0b37ad3d3ed123
tanxuejiao <1534147975@qq.com>
2019-08-28 00:07:52 +0800

    header main

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -11,10 +11,15 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  height: 100vh;
 }
 </style>
