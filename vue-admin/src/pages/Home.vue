98255c98f44ee5fb6fe9f5cfb5f4f7b8166f936a
txj <1534147975@qq.com>
2019-08-27 11:29:25 +0800

    home page

---
+++ vue-admin/src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+          <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>导航一</template>
+              <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+          </el-submenu>
+      </el-aside>
+  </el-container>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- vue-admin/src/router/index.js
+++ vue-admin/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
+import Home from '@/pages/Home'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
